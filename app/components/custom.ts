7ef4df2b3f72a6999c24abc06225871a63f3e21e
zhangxiang <zhangxiang@myhexin.com>
2023-07-07 17:02:19 +0800

    尝试解决问题

--- app/components/custom.ts
+++ app/components/custom.ts
@@ -84,6 +84,7 @@ function loginMyServer(completeBlock: (arg0: any) => void) {
         // if (typeof alert !== 'undefined') {
         //   alert("检测到更换设备登录，需要把服务器数据覆盖到本地，如果不想覆盖本地数据，不要点击确定按钮，请立即关闭该页面，否则数据会自动覆盖！！！");
         // }
+        
         // window.setTimeout(() => {
         //   location.reload();
         // }, 1000);
